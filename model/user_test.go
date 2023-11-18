fb83101dedd39b68ce6948185387eea41b387828
Kwe <Kweayon@gmail.com>
2023-11-18 14:11:00 -0600

    update: added tests for entry, user and router

---
+++ model/user_test.go
@@ -0,0 +1,34 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// TODO: Review sqlmock
+// TODO: figureout how to test inserts
+// TODO: all methods that interact with the database should take the database as an argument
+func TestSaveUser(t *testing.T) {
+
+	mockDB, _, _ := sqlmock.New()
+
+	defer mockDB.Close()
+
+	dialector := postgres.New(postgres.Config{
+		Conn:       mockDB,
+		DriverName: "postgres",
+	})
+
+	user := User{
+		Username: "kwe92",
+		Password: "Ronin",
+	}
+
+	db, _ := gorm.Open(dialector, &gorm.Config{})
+
+	user.Save(db)
+
+}
